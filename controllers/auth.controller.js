20ecf496489e68eb4d03728553b8948bb460327d
Leandro Arturi <lea.arturi@gmail.com>
2022-08-08 11:48:47 -0300

    Login y create user google auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
-const { request, response } = require('express');
+const { request, response, json } = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require( '../helpers/google-verify' );
 
 const login = async(req = request, res = response) => {
 
@@ -40,13 +41,57 @@ const login = async(req = request, res = response) => {
     }
 }
 
-const googleSignIn = (req, res = response) => {
+const googleSignIn = async (req, res = response) => {
     const { google_token } = req.body;
 
-    res.status(200).json({
-        msg: 'Todo ok',
-        google_token,
-    });
+    try {
+        const { name, picture, email } = await googleVerify(google_token);
+
+        let usuario = await User.findOne({ email });
+
+        // Creo el usuario
+        if (!usuario) {
+            const data = {
+                name,
+                email,
+                password: ':)',
+                img: picture,
+                google: true
+            };
+
+            usuario = new User(data);
+            await usuario.save();
+        }
+
+        // Si el usuario existe pero esta inhabilitado
+        if(!usuario.status){
+            return res.status(401).json({
+                msg: 'El usuario esta bloqueado. Hable con el administrador.'
+            });
+        }
+
+        // Si el usuario existe, esta habilitado y no tenia cuenta de google, hago un update
+        if(usuario.status) {
+            usuario.google = true;
+            await usuario.save();
+        }
+
+        // Generar JWT
+        const token = await generarJWT(usuario.id)
+
+        return res.status(200).json({
+            ok: true,
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+    
 }
 
 module.exports = {
