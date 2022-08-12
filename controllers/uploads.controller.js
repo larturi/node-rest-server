9e650468be4466fb009b11e6d0f917f8fd0d4e76
Leandro Arturi <lea.arturi@gmail.com>
2022-08-12 16:10:12 -0300

    Upload files - Cloudinary

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 const { response } = require('express');
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const { uploadFile } = require('../helpers');
 const { Product, User } = require('../models')
 
@@ -91,8 +95,52 @@ const showImage = async (req, res = response) => {
     return res.sendFile(pathImagenPlaceholder);
 }
 
+const actualizarImagenCloudinary = async (req, res = response) => {
+    const { collection, id } = req.params;
+
+    let modelo;
+
+    switch (collection) {
+        case 'users':
+            modelo = await User.findById(id);
+            if(!modelo) {
+                return res.status(400).json({ msg: `No existe un usuario con el id ${id}` });
+            }
+        break;
+
+        case 'products':
+            modelo = await Product.findById(id);
+            if(!modelo) {
+                return res.status(400).json({ msg: `No existe un producto con el id ${id}` });
+            }
+        break;
+    
+        default:
+            return res.status(500).json({ msg: 'Colleccion no soportada' })
+    }
+
+    // Elimina la imagen vieja
+    if(modelo.img) {
+        const nameArr = modelo.img.split('/');
+        const nombre = nameArr[nameArr.length - 1];
+        const [ public_id ] = nombre.split('.');
+        cloudinary.uploader.destroy(public_id);
+    }
+
+    // Guarda la imagen
+    const { tempFilePath } = req.files.file;
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+    modelo.img = secure_url;
+
+    await modelo.save();
+
+    return res.status(400).json(modelo);
+    
+}
+
 module.exports = {
     subirArchivo,
     actualizarImagen,
     showImage,
+    actualizarImagenCloudinary,
 }
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,28 +1,52 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { subirArchivo, actualizarImagen, showImage } = require( '../controllers/uploads.controller' );
-const { validCollections } = require( '../helpers' );
-const { validarCampos, validarArchivoUpload } = require( '../middlewares' );
+const {
+   subirArchivo,
+   actualizarImagen,
+   showImage,
+   actualizarImagenCloudinary,
+} = require('../controllers/uploads.controller');
+const { validCollections } = require('../helpers');
+const { validarCampos, validarArchivoUpload } = require('../middlewares');
 
 const router = Router();
 
 // HACE EL UPLOAD DE UN FILE
 router.post('/', validarArchivoUpload, subirArchivo);
 
-// HACE EL UPDATE DE UN FILE
-router.put('/:collection/:id', [
-    validarArchivoUpload,
-    check('id', 'No es un ID valido').isMongoId(),
-    check('collection').custom(c => validCollections(c, ['users', 'products'])),
-    validarCampos
-], actualizarImagen);
+// HACE EL UPDATE DE UN FILE EN SERVER LOCAL
+// router.put('/:collection/:id', [
+//     validarArchivoUpload,
+//     check('id', 'No es un ID valido').isMongoId(),
+//     check('collection').custom(c => validCollections(c, ['users', 'products'])),
+//     validarCampos
+// ], actualizarImagen);
+
+// HACE EL UPDATE DE UN FILE EN CLOUDINARY
+router.put(
+   '/:collection/:id',
+   [
+      validarArchivoUpload,
+      check('id', 'No es un ID valido').isMongoId(),
+      check('collection').custom((c) =>
+         validCollections(c, ['users', 'products'])
+      ),
+      validarCampos,
+   ],
+   actualizarImagenCloudinary
+);
 
 // MOSTRAR UNA IMAGEN
-router.get('/:collection/:id', [
-    check('id', 'No es un ID valido').isMongoId(),
-    check('collection').custom(c => validCollections(c, ['users', 'products'])),
-    validarCampos
-], showImage);
-
+router.get(
+   '/:collection/:id',
+   [
+      check('id', 'No es un ID valido').isMongoId(),
+      check('collection').custom((c) =>
+         validCollections(c, ['users', 'products'])
+      ),
+      validarCampos,
+   ],
+   showImage
+);
 
 module.exports = router;
